4bf38ee6cf9ac4a5fbff263487896d3ab854d49d
Johannes Egger <eggerjohansi@gmail.com>
2014-12-07 23:49:00 +0100

    Refactor tests to use string-based sample code.
    This should increase readability.

--- ExtractTypeToFileDiagnostic/ExtractTypeToFileDiagnostic.Test/UnitTests.cs
+++ ExtractTypeToFileDiagnostic/ExtractTypeToFileDiagnostic.Test/UnitTests.cs
@@ -29,12 +29,19 @@ namespace ExtractTypeToFileDiagnostic.Test
         [Test]
         public async Task ShouldRenameFileWhenTypeNameDoesNotMatchFileName()
         {
-            var content = CreateSampleContent();
+            var content =
+@"using System;
+using System.Linq;
+
+namespace TestNamespace
+{
+    class TypeA { }
+}";
 
             var project = GetSampleProject();
             var documentId = DocumentId.CreateNewId(project.Id);
             var document = project.Solution
-                .AddDocument(documentId, "Class1.cs", content.GetText())
+                .AddDocument(documentId, "Class1.cs", content)
                 .GetDocument(documentId);
 
             var expectedDiagnostic = new DiagnosticResult
@@ -42,7 +49,7 @@ namespace ExtractTypeToFileDiagnostic.Test
                 Id = ExtractTypeToFileAnalyzer.DiagnosticId,
                 Message = string.Format(ExtractTypeToFileAnalyzer.MessageFormat, "TypeA", "Class1"),
                 Severity = DiagnosticSeverity.Warning,
-                Locations = new[] { new DiagnosticResultLocation("Class1.cs", 11, 11) }
+                Locations = new[] { new DiagnosticResultLocation("Class1.cs", 6, 11) }
             };
 
             var analyzer = GetCSharpDiagnosticAnalyzer();
@@ -50,7 +57,7 @@ namespace ExtractTypeToFileDiagnostic.Test
             DiagnosticVerifier.VerifyDiagnosticResults(diagnostics, analyzer, expectedDiagnostic);
 
             var newDocument = project.Solution
-                .AddDocument(documentId, "TypeA.cs", content.GetText())
+                .AddDocument(documentId, "TypeA.cs", content)
                 .GetDocument(documentId);
             var actualSolution = await CodeFixVerifier.ApplyFixAsync(GetCSharpCodeFixProvider(), document, diagnostics.Single());
 
@@ -63,14 +70,21 @@ namespace ExtractTypeToFileDiagnostic.Test
         [Test]
         public async Task ShouldNotProduceDiagnosticForNestedType()
         {
-            var content = SyntaxFactory.CompilationUnit()
-                .AddMembers(SyntaxFactory.ClassDeclaration("TypeA")
-                    .AddMembers(SyntaxFactory.ClassDeclaration("TypeB")));
+            var content =
+@"using System;
+using System.Linq;
+
+namespace TestNamespace
+{
+    class TypeA {
+        class TypeB { }
+    }
+}";
 
             var project = GetSampleProject();
             var documentId = DocumentId.CreateNewId(project.Id);
             var document = project.Solution
-                .AddDocument(documentId, "TypeA.cs", content.GetText())
+                .AddDocument(documentId, "TypeA.cs", content)
                 .GetDocument(documentId);
 
             var analyzer = GetCSharpDiagnosticAnalyzer();
@@ -81,24 +95,20 @@ namespace ExtractTypeToFileDiagnostic.Test
         [Test]
         public async Task ShouldExtractTypeWhenFileContainsMultipleTypes()
         {
-            var content = (CompilationUnitSyntax)CreateSampleContent();
-            content = content.WithMembers(
-                SyntaxFactory.List(new MemberDeclarationSyntax[] {
-                    content
-                    .ChildNodes()
-                    .OfType<NamespaceDeclarationSyntax>()
-                    .Single()
-                    .AddMembers(content
-                        .DescendantNodes()
-                        .OfType<ClassDeclarationSyntax>()
-                        .Single()
-                        .WithIdentifier(SyntaxFactory.ParseToken("TypeB")))
-                }));
+            var content =
+@"using System;
+using System.Linq;
+
+namespace TestNamespace
+{
+    class TypeA { }
+    class TypeB { }
+}";
 
             var project = GetSampleProject();
             var documentAId = DocumentId.CreateNewId(project.Id);
             var document = project.Solution
-                .AddDocument(documentAId, "TypeA.cs", content.GetText())
+                .AddDocument(documentAId, "TypeA.cs", content)
                 .GetDocument(documentAId);
 
             var expectedDiagnostic = new DiagnosticResult
@@ -106,34 +116,35 @@ namespace ExtractTypeToFileDiagnostic.Test
                 Id = ExtractTypeToFileAnalyzer.DiagnosticId,
                 Message = string.Format(ExtractTypeToFileAnalyzer.MessageFormat, "TypeB", "TypeA"),
                 Severity = DiagnosticSeverity.Warning,
-                Locations = new[] { new DiagnosticResultLocation("TypeA.cs", 12, 11) }
+                Locations = new[] { new DiagnosticResultLocation("TypeA.cs", 7, 11) }
             };
 
             var analyzer = GetCSharpDiagnosticAnalyzer();
             var diagnostics = await DiagnosticVerifier.GetSortedDiagnosticsFromDocumentsAsync(analyzer, document);
             DiagnosticVerifier.VerifyDiagnosticResults(diagnostics, analyzer, expectedDiagnostic);
 
-            var extractedContent = (CompilationUnitSyntax)CreateSampleContent();
-            extractedContent = extractedContent.WithMembers(
-                SyntaxFactory.List(new MemberDeclarationSyntax[] {
-                    extractedContent
-                    .ChildNodes()
-                    .OfType<NamespaceDeclarationSyntax>()
-                    .Single()
-                    .WithMembers(
-                        SyntaxFactory.List(new MemberDeclarationSyntax[] {
-                            extractedContent
-                            .DescendantNodes()
-                            .OfType<ClassDeclarationSyntax>()
-                            .Single()
-                            .WithIdentifier(SyntaxFactory.ParseToken("TypeB"))
-                        }))
-                }));
+            var expectedTypeAContent =
+@"using System;
+using System.Linq;
+
+namespace TestNamespace
+{
+    class TypeA { }
+}";
+
+            var expectedTypeBContent =
+@"using System;
+using System.Linq;
+
+namespace TestNamespace
+{
+    class TypeB { }
+}";
 
             var documentBId = DocumentId.CreateNewId(project.Id);
             var expectedSolution = project.Solution
-                .AddDocument(documentAId, "TypeA.cs", CreateSampleContent().GetText())
-                .AddDocument(documentBId, "TypeB.cs", extractedContent.GetText());
+                .AddDocument(documentAId, "TypeA.cs", expectedTypeAContent)
+                .AddDocument(documentBId, "TypeB.cs", expectedTypeBContent);
             var actualSolution = await CodeFixVerifier.ApplyFixAsync(GetCSharpCodeFixProvider(), document, diagnostics.Single());
 
             await CodeFixVerifier.VerifyFix(expectedSolution, actualSolution, documentBId);
@@ -142,22 +153,6 @@ namespace ExtractTypeToFileDiagnostic.Test
             DiagnosticVerifier.VerifyDiagnosticResults(diagnostics2, analyzer, new DiagnosticResult[0]);
             var diagnostics3 = await DiagnosticVerifier.GetSortedDiagnosticsFromDocumentsAsync(analyzer, expectedSolution.GetDocument(documentBId));
             DiagnosticVerifier.VerifyDiagnosticResults(diagnostics2, analyzer, new DiagnosticResult[0]);
-        }
-
-        private static SyntaxNode CreateSampleContent()
-        {
-            return SyntaxFactory.ParseCompilationUnit(@"
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Diagnostics;
-
-namespace TestNamespace
-{{
-    class TypeA {{ }}
-}}");
         }
 
         private static Project GetSampleProject()
